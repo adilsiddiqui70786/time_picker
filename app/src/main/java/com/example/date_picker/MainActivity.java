1f3e3f52e9048dc824301fa40db4a4ff39ce3747
Mohd Adil <117959085+adilsiddiqui70786@users.noreply.github.com>
2023-03-31 19:30:55 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/date_picker/MainActivity.java
+++ app/src/main/java/com/example/date_picker/MainActivity.java
@@ -6,21 +6,25 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity 
+{
     TimePicker timePicker;
     TextView textView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         timePicker = findViewById(R.id.timing);
         textView = findViewById(R.id.textview);
 
-        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener()
+        {
             @Override
-            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) 
+            {
                 textView.setText("Hour: " +hourOfDay +" " +minute);
             }
         });
